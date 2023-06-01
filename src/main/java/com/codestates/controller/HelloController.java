45d0835fd2860441fe89fd0a46e597f8aa288ad6
gibbeunLee <gibbeunlee@gmail.com>
2023-06-01 18:33:43 +0900

    gradle reload

--- src/main/java/com/codestates/controller/HelloController.java
+++ src/main/java/com/codestates/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.codestates.practicegithubActiondeploy.controller;
+package com.codestates.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
